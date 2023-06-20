7bb6cc297467288749dfe9070c3a91809d8e20d5
prudhvic <prudhvichinnam101@gmail.com>
2023-06-20 21:50:04 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <Skills />
         <Projects />
+        <Contact />
       </main>
     </>
   )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import { AiOutlineSend, AiFillMail } from "react-icons/ai";
 const Contact = () => {
   return (
-    <form>
-        <input type="text"/>
-        
-    </form>
+    <div className='contact' id='contact'>
+      <p>mailto:<a href="mailto:prudhvichinnam101@gmail.com">prudhvichinnam101@gmail.com</a> <AiFillMail color='white'/></p>
+      <a href="mailto:prudhvichinnam101@gmail.com">send <AiOutlineSend color='white' /></a>
+    </div>
   )
 }
 
