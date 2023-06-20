d32aaa2768237c00c0d2b48447d84fd80518161e
prudhvic <prudhvichinnam101@gmail.com>
2023-06-20 21:01:37 +0530

    initial

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-// import Rings from "./rings"
+import Rings from "./rings"
 
 const Hero = () => {
     return (
@@ -6,7 +6,7 @@ const Hero = () => {
             <section className='hero-section'>
                 <div className="content">
                     <Rings />
-                    <h2><strong>Nice To meet You!</strong> I'm Prudhvi</h2>
+                    <h2><strong>Nice To meet You!</strong>I{"'"}m Prudhvi</h2>
                     <p className='leading-text'>
                         As a full-stack developer, I possess a diverse set of skills and expertise in both front-end and back-end web development. My portfolio showcases my ability to create visually appealing and user-friendly interfaces using HTML, CSS, and JavaScript frameworks. I also demonstrate proficiency in server-side programming languages like Node.js, along with expertise in database management. With a focus on delivering end-to-end solutions, I highlight my experience in software architecture, version control, and deployment strategies. My portfolio reflects my versatility and problem-solving abilities in developing comprehensive web applications.
                     </p>
